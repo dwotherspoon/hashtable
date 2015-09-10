f96ebd470aae3d95169ac22379d5b2eae1eb956f
David Wotherspoon <david@dwotherspoon.com>
2015-09-10 20:40:52 +0100

    Convert to use stdint, simplify structure.

--- hashtable.c
+++ hashtable.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <hashtable.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -37,8 +38,8 @@
 
 #define DJBX_ROUND(h, k) hash = ((h << 5) + h) + *k++;
 
-static unsigned long inline hash_djbx(const char * key, unsigned int len) {
-	register unsigned long hash = 5381;
+static uint64_t inline hash_djbx(const char * key, uint32_t len) {
+	register uint64_t hash = 5381;
 	for (;len >= 8; len -= 8) {
 		DJBX_ROUND(hash, key);
 		DJBX_ROUND(hash, key);
@@ -49,7 +50,7 @@ static unsigned long inline hash_djbx(const char * key, unsigned int len) {
 		DJBX_ROUND(hash, key);
 		DJBX_ROUND(hash, key);
 	}
-	/* Do remaining rounds. (n.b. fall through) */
+	/* Do remaining rounds. (n.b. fallthrough) */
 	switch (len) {
 		case 7: DJBX_ROUND(hash, key);
 		case 6: DJBX_ROUND(hash, key);
@@ -66,15 +67,15 @@ static unsigned long inline hash_djbx(const char * key, unsigned int len) {
 /* Define so we can switch hash funcs easily.*/
 #define HASH(k, l) hash_djbx(k, l)
 
-void hashtable_init(HashTable * table, unsigned int size) {
+void hashtable_init(HashTable * table, uint32_t size) {
 	/* Find power of two greater than size. Min size is 8 elems. */
-	unsigned int i = 3;
-	for (;(1U << i) < size; i++);
-	table->num_elements = 0;
+	uint32_t i = 3;
+	for (;((uint32_t)1 << i) < size; i++);
+	table->count = 0;
 	table->size = 1 << i;
 	table->mask = table->size - 1;
 	printf("Init: HashTable with size %u (requested %u).\n", table->size, size);
-	table->entries = calloc(table->size, sizeof(Entry));
+	table->entries = calloc(table->size, sizeof(Entry *));
 }
 
 void hashtable_deinit(HashTable * table) {
@@ -87,140 +88,139 @@ void hashtable_resize(HashTable * table) {
 }
 
 void hashtable_debug(HashTable * table) {
-	unsigned int i;
-	Item * pItem;
+	uint32_t i;
+	Entry * pEntry;
 	for (i = 0; i < table->size; i++) {
-		pItem = table->entries[i].pHead;
+		pEntry = table->entries[i];
 		printf("%i => ", i);
-		while (pItem != NULL) {		
-			printf("%p, ", pItem);
-			pItem = pItem->pNext;
+		while (pEntry != NULL) {		
+			printf("%p, ", pEntry);
+			pEntry = pEntry->pNext;
 		} 
 		puts(" NULL.");
 	}
 }
 
 /* Compare an entry, with full hash, key and length. */
-static int inline hashtable_entry_test(Item * i, unsigned long hash, const char * key, unsigned int len) {
+static int inline hashtable_entry_test(Entry * pEntry, uint64_t hash, const char * key, uint32_t len) {
 	/* Always false for null items. */
-	if (i == NULL) {
+	if (pEntry == NULL) {
 		return 0;
 	}
 	/* Compare unmasked hash and key length. */
-	if ((i->hash != hash) || (i->key_len != len)) {
+	if ((pEntry->hash != hash) || (pEntry->key_len != len)) {
 		return 0;
 	}
-	/* Compare keys */
-	if (memcmp(key, i->key, len) != 0) { 
+	/* Compare keys. */
+	if (memcmp(key, pEntry->key, len) != 0) { 
 		return 0;
 	}
 	return 1;
 }
 
-/* Return pointer to value in table or null if not in table. */
-void * hashtable_get(HashTable * table, const char * key, unsigned int len) {
-	Item * pItem;
-	unsigned long hash = HASH(key, len);
+/* Return pointer to value in table or NULL if not in table. */
+void * hashtable_get(HashTable * table, const char * key, uint32_t len) {
+	Entry * pEntry;
+	void * result = NULL;
+	uint64_t hash = HASH(key, len);
 	/* TODO: Check if table is init'd. */
 	printf("Get: key %s hashed to %lu.\n", key, hash);
-	pItem = table->entries[hash & table->mask].pHead;
-	/* Is this a list? */
-	printf("Get: chain head @ %p.\n", pItem);
-	while (pItem != NULL) {
-		if (hashtable_entry_test(pItem, hash, key, len)) {
-			return pItem->value;
+	pEntry = table->entries[hash & table->mask];
+	printf("Get: chain head @ %p.\n", pEntry);
+	while (!result && pEntry) {
+		if (hashtable_entry_test(pEntry, hash, key, len)) {
+			result = pEntry->value;
 		}
-		pItem = pItem->pNext;
+		pEntry = pEntry->pNext;
 	}
-	return NULL;
+	return result;
 }
 
-/* Find and delete a value in table. Returns pointer to value or NULL */
-void * hashtable_unset(HashTable * table, const char * key, unsigned int len) {
+/* Find and delete a value in table. Returns pointer to value or NULL. */
+void * hashtable_unset(HashTable * table, const char * key, uint32_t len) {
 	Entry * pEntry;
-	Item * pItem;
 	void * result = NULL;
-	unsigned long hash = HASH(key, len);
+	uint64_t hash = HASH(key, len);
 	/* TODO: Check if table is init'd. */
-	pEntry = &(table->entries[hash & table->mask]);
-	pItem = pEntry->pHead;
-	while (pItem != NULL) {
-		if (hashtable_entry_test(pItem, hash, key, len)) {
-			break;
-		}
-		pItem = pItem->pNext;
-	}
-	if (pItem != NULL) {
-		printf("Delete: found item @ %p.\n", pItem);
-		if (pItem->pPrev == NULL) {
-			/* Item is the head. */
-			pEntry->pHead = pItem->pNext;
+	pEntry = table->entries[hash & table->mask];
+	/* Search chain for entry. */
+	for (;!hashtable_entry_test(pEntry, hash, key, len) && pEntry; pEntry = pEntry->pNext);
+	/* If pEntry is not NULL, entry has been found */
+	if (pEntry) {
+		printf("Unset: found item @ %p.\n", pEntry);
+		if (!pEntry->pPrev) {
+			/* Entry is the head. */
+			table->entries[hash & table->mask] = pEntry->pNext;
 		}
 		else {
-			pItem->pPrev->pNext = pItem->pNext;
+			/* Set next of prev element. */
+			pEntry->pPrev->pNext = pEntry->pNext;
 		}
-		if (pItem->pNext == NULL) {
-			/* Item is the tail. */
-			pEntry->pTail = pItem->pPrev;
-		}
-		else {
-			pItem->pNext->pPrev = pItem->pPrev;
+		if (pEntry->pNext) {
+			/* Entry is not the tail - set prev of next element. */
+			pEntry->pNext->pPrev = pEntry->pPrev;
 		}
 		/* Free memory. */
-		result = pItem->value;
-		free(pItem->key);
-		free(pItem);
-		table->num_elements--;
+		result = pEntry->value;
+		free(pEntry->key);
+		free(pEntry);
+		table->count--;
 	}
-	return  result;
+	return result;
 }
 
 /* Insert or update existing value. */
-void hashtable_set(HashTable * table, const char * key, unsigned int len, void * value) {
+void hashtable_set(HashTable * table, const char * key, uint32_t len, void * value) {
 	Entry * pEntry;
-	Item * pItem;
+	/* 0 = Chain is empty, 1 = Found, 2 = Hit tail without finding. */
+	int action = 0; 
 	unsigned long hash = HASH(key, len);
 	printf("Insert: key %s hashed to %lu.\n", key, hash);
 	/* TODO: Check if table is init'd. */
 	/* TODO: Resize if needed. */
-	pEntry = &(table->entries[hash & table->mask]);
-	pItem = pEntry->pHead;
-
-	while (pItem != NULL) {
-		if (hashtable_entry_test(pItem, hash, key, len)) {
-			break;
-		}		
-		pItem = pItem->pNext;
+	pEntry = table->entries[hash & table->mask];
+	/* Search chain. */
+	while (!action && pEntry) {
+		if (hashtable_entry_test(pEntry, hash, key, len)) {
+			action = 1;
+		}
+		else if (!pEntry->pNext) {
+			/* The current item is the tail. */
+			action = 2;
+		}
+		else {
+			pEntry = pEntry->pNext;
+		}
 	}
 
-	if (pItem == NULL) {
-		puts("Set: inserting value.");
-		pItem = malloc(sizeof(Item));
-		if (pEntry->pTail != NULL) {
-			/* Make tail item's next point to new item. */
-			pEntry->pTail->pNext = pItem;
+	if (action != 1) {
+		if (action == 0) {
+			/* Empty chain insert. */
+			puts("Set: inserting value (head).");
+			pEntry = malloc(sizeof(Entry));
+			table->entries[hash & table->mask] = pEntry;
+			pEntry->pPrev = NULL;
 		}
-		/* Make this item point to list tail (whether NULL or not). */
-		pItem->pPrev = pEntry->pTail;
-		/* This item is the new tail */
-		pEntry->pTail = pItem;
-		/* This item points to nothing. */
-		pItem->pNext = NULL;
-		if (pEntry->pHead == NULL) {
-			/* This is the first in the list. */
-			pEntry->pHead = pItem;
-			pItem->pPrev = NULL;
+		else {
+			/* Non-empty chain insert. */
+			puts("Set: inserting value.");
+			pEntry->pNext = malloc(sizeof(Entry));
+			/* Point new entry back at tail. */
+			pEntry->pNext->pPrev = pEntry;
+			pEntry = pEntry->pNext;
 		}
-		pItem->hash = hash;
-		pItem->key = malloc(len * sizeof(char));
-		pItem->key_len = len;
-		memcpy(pItem->key, key, len * sizeof(char));
-		table->num_elements++;
+		pEntry->pNext = NULL;
+		pEntry->hash = hash;
+		pEntry->key = malloc(len * sizeof(char));
+		pEntry->key_len = len;
+		memcpy(pEntry->key, key, len * sizeof(char));
+		table->count++;
 	}
 	else {
 		puts("Set: updating value.");
 	}
-	pItem->value = value;
+	/* Set value. */
+	pEntry->value = value;
 }
 
 /* Iterator support */
@@ -228,36 +228,28 @@ void hashtable_set(HashTable * table, const char * key, unsigned int len, void *
 /* Set the internal pointers to the first element in the table. */
 void hashtable_iter_first(HashTable * table) {
 	unsigned int pos;
-	if (table->num_elements) {
+	if (table->count) {
 		/* We know there's an element, iterate until we find it. */
-		for (pos = 0; (pos < table->num_elements) && (table->entries[pos].pHead != NULL); pos++);
+		for (pos = 0; (pos < table->size) && table->entries[pos]; pos++);
 		/* Set pointers. */
-		table->pEntry = &(table->entries[pos]);
-		table->pItem = table->pEntry->pHead;
+		table->pEntry = table->entries[pos];
 	}
 }
 
 /* Set the internal pointers to the next element in the table. */
 void hashtable_iter_next(HashTable * table) {
-	if (table->pEntry && table->pItem) {
-		if (table->pItem->pNext) {
-			/* Simply move to next. */
-		}
-		else {
-			/* Search for next occupied entry. */
-		}
-	}
+	
 }
 
 /* Consider making these macros. */
 char * hashtable_iter_key(HashTable * table) {
-	return (table == NULL || table->pItem == NULL) ? (char *)NULL : table->pItem->key;
+	return (table == NULL || table->pEntry == NULL) ? (char *)NULL : table->pEntry->key;
 }
 
 unsigned int hashtable_iter_key_len(HashTable * table) {
-	return (table == NULL || table->pItem == NULL) ? 0 : table->pItem->key_len;
+	return (table == NULL || table->pEntry == NULL) ? 0 : table->pEntry->key_len;
 }
 
 void * hashtable_iter_value(HashTable * table) {
-	return (table == NULL || table->pItem == NULL) ? NULL : table->pItem->value;
+	return (table == NULL || table->pEntry == NULL) ? NULL : table->pEntry->value;
 }
--- include/hashtable.h
+++ include/hashtable.h
@@ -1,38 +1,32 @@
-
-typedef struct item_t {
-	unsigned long hash;
-	char * key;
-	unsigned int key_len;
-	void * value;
-	struct item_t * pPrev;
-	struct item_t * pNext;
-} Item;
-
 typedef struct entry_t {
-	Item * pHead;
-	Item * pTail;
+	uint64_t hash;
+	char * key;
+	uint32_t key_len;
+	void * value;
+	struct entry_t * pPrev;
+	struct entry_t * pNext;
 } Entry;
 
 typedef struct hashtable_t {
 	/* Number of elements in array. */
-	unsigned int num_elements;
+	uint32_t count;
 	/* Size of the table. */
-	unsigned int size;
+	uint32_t size;
 	/* Table mask. */
-	unsigned int mask;
+	uint32_t mask;
 	/* Entries array (main storage). */
-	Entry * entries;
+	Entry ** entries;
 
 	/* Pointer to current Entry (for iterator). */
 	Entry * pEntry;
-	/* Pointer to current item in pEntry (for iterator). */
-	Item * pItem;
+	/* Current position in entries (for iterator). */
+	uint32_t pos_entries;
 } HashTable;
 
-void hashtable_init(HashTable * table, unsigned int size);
+void hashtable_init(HashTable * table, uint32_t size);
 void hashtable_deinit(HashTable * table);
 void hashtable_debug(HashTable * table);
 
-void * hashtable_get(HashTable * table, const char * key, unsigned int len);
-void hashtable_set(HashTable * table, const char * key, unsigned int len, void * value);
-void * hashtable_unset(HashTable * table, const char * key, unsigned int len);
+void * hashtable_get(HashTable * table, const char * key, uint32_t len);
+void hashtable_set(HashTable * table, const char * key, uint32_t len, void * value);
+void * hashtable_unset(HashTable * table, const char * key, uint32_t len);
